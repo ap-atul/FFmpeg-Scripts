69274c0dcd0d8789af508b5f0c3493a2f6b1479c
Atul Patare <atulpatare99@gmail.com>
2021-05-11 00:55:40 +0530

    Update gifCreation.sh

--- gifCreation.sh
+++ gifCreation.sh
@@ -1,15 +1,8 @@
 # This one should be executed in steps
-# check the scale before use
 
-# extract frames from vid
-ffmpeg -i vid.mp4 %d.png
+# generate a palette
+ffmpeg -i vid.mp4 -filter_complex "[0:v] palettegen" palette.png
 
-# create a pallete of all images (all file should be named as 1.png 2.png 3.png)
-ffmpeg -f image2 -i %d.png -vf scale=512:-1:sws_dither=ed,palettegen palette.png
-
-# creating a video out of all the images
-ffmpeg -f image2 -framerate 1.2 -i %d.png video.flv
-
-# using the pallete to create looping video gif
-# change the scale and fps to control the look
-ffmpeg -i video.flv -i palette.png -filter_complex "fps=1.2,scale=512:-1:flags=lanczos[x];[x][1:v]paletteuse" gifCreated.gif
+# generate gif using palette
+ffmpeg -i vid.mp4 -i palette.png -filter_complex "[0:v][1:v] paletteuse" out.gif
+ffmpeg -i vid.mp4 -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" out.gif
