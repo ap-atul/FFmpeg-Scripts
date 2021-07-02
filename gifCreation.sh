b96d4545e02425db6878dc2b4c92a683ae06cb5b
Atul Patare <atulpatare99@gmail.com>
2021-07-02 22:12:00 +0530

    preserves transparency

--- gifCreation.sh
+++ gifCreation.sh
@@ -1,8 +1,7 @@
 # This one should be executed in steps
 
 # generate a palette
-ffmpeg -i vid.mp4 -filter_complex "[0:v] palettegen" palette.png
+ffmpeg -f image2 -framerate 2 -i %d.png -vf palettegen palette.png
 
-# generate gif using palette
-ffmpeg -i vid.mp4 -i palette.png -filter_complex "[0:v][1:v] paletteuse" out.gif
-ffmpeg -i vid.mp4 -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" out.gif
+# generate gif using palette (modify the scale options and framerate)
+ffmpeg -f image2 -framerate 2 -i %d.png -i palette.png -filter_complex "[0]scale=480:-1[j];[j][1] paletteuse" out.gif
